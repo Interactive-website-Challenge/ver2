fccfe261126123e2529f542f28b4d185999afdd3
Giorgia <giorgiashogbanmu@hotmail.it>
2024-12-07 18:49:21 +0000

    js not working fixed

--- js/validation.js
+++ js/validation.js
@@ -4,15 +4,21 @@ const email_input = document.getElementById("email-input");
 const password_input = document.getElementById("password-input");
 const repeat_password_input = document.getElementById("repeat-password-input");
 
-form.addEventListener("sumbit", (e) =>{
+form.addEventListener("submit", (e) =>{
     // e.preventDefault()
-    let errors = []
+    let errors = [];
 
     if(firstname_input) {
         errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value);
     } else {
         errors = getLoginFormErrors(email_input.value, password_input.value);
     }
+
+    if(errors.length > 0) {
+        e.preventDefault();
+    }
+
+
 }) 
 
 function getSignupFormErrors(firstname, email, password, repeatPassword) {
@@ -20,26 +26,28 @@ function getSignupFormErrors(firstname, email, password, repeatPassword) {
 
     if(firstname === "" || firstname == null) {
         errors.push("Firstname is required");
-        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        firstname_input.parentElement.classList.add("incorrect"); /*after error occurs, this adds the class incorrect to the div, which activates the css 
         code for the incorrect class*/
     }
 
     if(email === "" || email == null) {
         errors.push("Email is required");
-        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        email_input.parentElement.classList.add("incorrect"); /*after error occurs, this adds the class incorrect to the div, which activates the css 
         code for the incorrect class*/
     }
 
     if(password === "" || password == null) {
         errors.push("Password is required");
-        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        password_input.parentElement.classList.add("incorrect"); /*after error occurs, this adds the class incorrect to the div, which activates the css 
         code for the incorrect class*/
     }
 
     if(repeatPassword === "" || repeatPassword  == null) {
         errors.push("Password Repeat is required");
-        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        repeat_password_input.parentElement.classList.add("incorrect"); /*after error occurs, this adds the class incorrect to the div, which activates the css 
         code for the incorrect class*/
     }
+
+    return errors;
 
 }
