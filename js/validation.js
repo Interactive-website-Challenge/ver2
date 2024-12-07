e1152ffcd609ec558e120513a20a432a8fb3123a
Giorgia <giorgiashogbanmu@hotmail.it>
2024-12-07 18:26:47 +0000

    js errors

--- js/validation.js
+++ js/validation.js
@@ -14,3 +14,32 @@ form.addEventListener("sumbit", (e) =>{
         errors = getLoginFormErrors(email_input.value, password_input.value);
     }
 }) 
+
+function getSignupFormErrors(firstname, email, password, repeatPassword) {
+    let errors = [];
+
+    if(firstname === "" || firstname == null) {
+        errors.push("Firstname is required");
+        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        code for the incorrect class*/
+    }
+
+    if(email === "" || email == null) {
+        errors.push("Email is required");
+        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        code for the incorrect class*/
+    }
+
+    if(password === "" || password == null) {
+        errors.push("Password is required");
+        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        code for the incorrect class*/
+    }
+
+    if(repeatPassword === "" || repeatPassword  == null) {
+        errors.push("Password Repeat is required");
+        firstname_input.parentElement.classList.add("incorrect") /*after error occurs, this adds the class incorrect to the div, which activates the css 
+        code for the incorrect class*/
+    }
+
+}
