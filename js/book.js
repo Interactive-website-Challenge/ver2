92e833988fe9204c0b7eab49d7c531fb06adc9f5
Giorgia <106474194+giorgia834@users.noreply.github.com>
2024-12-10 15:53:26 +0000

    event buttons

--- js/book.js
+++ js/book.js
@@ -1,7 +1,7 @@
 const fullname_input = document.getElementById("name");
 
 function book() {
-    alert("Hey " + fullname_input.value.trim().toLowerCase().charAt(0).toUpperCase() + ", your ticket has been booked. We look forward to see you at the event!")
+    alert("Hi " + fullname_input.value.trim().toLowerCase().charAt(0).toUpperCase() + ", your ticket has been booked. We look forward to see you at the event!")
 }
 
 let booking_ticket = document.getElementById("book-now-button");
