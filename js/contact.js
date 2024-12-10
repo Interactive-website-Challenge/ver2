dd671140753d999a2fbadcae130f32e9b2c256ad
Giorgia <106474194+giorgia834@users.noreply.github.com>
2024-12-10 15:27:05 +0000

    contact js + about format

--- js/contact.js
+++ js/contact.js
@@ -4,5 +4,5 @@ function contact() {
     alert("Hey " + fullname_input.value.trim().toLowerCase().charAt(0).toUpperCase() + ", we have received your message, we will get back to you as soon as possible.")
 }
 
-let contact_message = document.getElementById("book-now-button");
+let contact_message = document.getElementById("contact-button");
 contact_message.addEventListener("click", contact);
