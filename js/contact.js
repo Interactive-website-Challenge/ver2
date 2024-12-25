eb87b6ae833fcdcea782a526f51ed801f23a3f7b
giorgia834 <106474194+giorgia834@users.noreply.github.com>
2024-12-25 21:12:09 +0000

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 const fullname_input = document.getElementById("name");
 
 function contact() {
-    alert("Hi " + fullname_input.value.trim().toLowerCase().charAt(0).toUpperCase() + ", we have received your message, we will get back to you as soon as possible.")
+    alert("Hi " + fullname_input.value.trim() + ", we have received your message, we will get back to you as soon as possible.")
 }
 
 let contact_message = document.getElementById("contact-button");
